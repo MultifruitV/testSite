e7b3179af88381cf931c71b8bca047b4f51183a2
MultifruitV <100833471+MultifruitV@users.noreply.github.com>
2022-04-10 20:22:07 +0300

    Add files
    Add all files

---
+++ Что-то/script.js
@@ -0,0 +1,6 @@
+document.getElementById('rus').onclick = function(){
+	window.location.href = 'rus.html';
+};
+document.getElementById('eng').onclick = function(){
+	window.location.href = 'eng.html';
+};
